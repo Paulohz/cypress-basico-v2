f21658543fa3e08d6e8a75a128afc5c206f14d8a
pahcosta <pahcosta@uolinc.com>
2024-05-06 20:38:49 -0300

    feat: Add ci pipeline

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,5 @@
 it('testa a página da política de privacidade de forma independente', () => {
     cy.visit('./src/privacy.html')
 
-    cy.contains('Talking about testing').should('be.visible')
+    cy.contains('Talking About Testing').should('be.visible')
 })
